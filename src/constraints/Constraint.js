938214dae6c3fc54a3a181faa3420ce91ca4005b
Stefan Hedman <schteppe@gmail.com>
2012-04-01 14:31:32 +0200

    Constraint class cleanup

--- src/constraints/Constraint.js
+++ src/constraints/Constraint.js
@@ -3,14 +3,15 @@
  * @author schteppe
  */
 CANNON.Constraint = function(){
-  this.J =        []; // Jacobian
-  this.iM =       []; // inverse mass matrix
-  this.q =        []; // Constraint violation
-  this.qdot =     []; // 
-  this.fext =     []; // External force
-  this.lambdamax = -Infinity; // Clamping for multipliers (see this as max constraint force)
-  this.lambdamin = Infinity;
-  this.bodyIds =  []; // Body id's to apply the constraint forces on
+  // n := number of subconstraints
+  this.J =          [];         // Jacobian, 12*n
+  this.iM =         [];         // Inverse mass matrix, 12*n
+  this.g =          [];         // Constraint violation, 12*n
+  this.gdot =       [];         // Derivative of g, 12*n
+  this.fext =       [];         // External force, 12*n
+  this.lambdamax = -Infinity;   // Clamping for multipliers (see this as max constraint force)
+  this.lambdamin =  Infinity;   
+  this.bodyIds =    [];         // Body id's to apply the constraint forces on. 2*n
 };
 
 CANNON.Constraint.prototype.constructor = CANNON.Constraint;
