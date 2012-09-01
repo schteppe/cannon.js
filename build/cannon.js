6fd88e27d8312ac61b571092daf6dcdb977e2028
Stefan Hedman <schteppe@gmail.com>
2012-09-01 20:26:44 +0200

    caching

--- build/cannon.js
+++ build/cannon.js
@@ -82,64 +82,66 @@ CANNON.NaiveBroadphase.prototype.constructor = CANNON.NaiveBroadphase;
  * @return array An array containing two arrays of integers. The integers corresponds to the body indices.
  */
 CANNON.NaiveBroadphase.prototype.collisionPairs = function(world){
-  var pairs1 = [], pairs2 = [];
-  var n = world.numObjects(),
-  bodies = world.bodies;
+    var pairs1 = [], pairs2 = [];
+    var n = world.numObjects(),
+    bodies = world.bodies;
 
-  // Local fast access
-  var types = CANNON.Shape.types;
-  var BOX_SPHERE_COMPOUND_CONVEX = types.SPHERE | types.BOX | types.COMPOUND | types.CONVEXPOLYHEDRON,
-  PLANE = types.PLANE,
-  STATIC_OR_KINEMATIC = CANNON.Body.STATIC | CANNON.Body.KINEMATIC;
+    // Local fast access
+    var types = CANNON.Shape.types;
+    var BOX_SPHERE_COMPOUND_CONVEX = types.SPHERE | types.BOX | types.COMPOUND | types.CONVEXPOLYHEDRON,
+    PLANE = types.PLANE,
+    STATIC_OR_KINEMATIC = CANNON.Body.STATIC | CANNON.Body.KINEMATIC;
 
-  // Temp vecs
-  var r = this.temp.r;
-  var normal = this.temp.normal;
-  var quat = this.temp.quat;
+    // Temp vecs
+    var temp = this.temp;
+    var r = temp.r,
+    normal = temp.normal,
+    quat = temp.quat;
 
-  // Naive N^2 ftw!
-  for(var i=0; i<n; i++){
-    for(var j=0; j<i; j++){
-      var bi = bodies[i], bj = bodies[j];
-      if(bi.shape && bj.shape){
-        var ti = bi.shape.type, tj = bj.shape.type;
+    // Naive N^2 ftw!
+    for(var i=0; i<n; i++){
+	for(var j=0; j<i; j++){
+	    var bi = bodies[i], bj = bodies[j];
+	    var bishape = bi.shape, bjshape = bj.shape;
+	    if(bishape && bjshape){
+		var ti = bishape.type, tj = bjshape.type;
 
-	if(((bi.motionstate & STATIC_OR_KINEMATIC)!==0 || bi.isSleeping()) &&
-	   ((bj.motionstate & STATIC_OR_KINEMATIC)!==0 || bj.isSleeping())) {
-  	    // Both bodies are static, kinematic or sleeping. Skip.
-	    continue;
+		if(((bi.motionstate & STATIC_OR_KINEMATIC)!==0 || bi.isSleeping()) &&
+		   ((bj.motionstate & STATIC_OR_KINEMATIC)!==0 || bj.isSleeping())) {
+  		    // Both bodies are static, kinematic or sleeping. Skip.
+		    continue;
+		}
+
+		// --- Box / sphere / compound / convexpolyhedron collision ---
+		if((ti & BOX_SPHERE_COMPOUND_CONVEX) && (tj & BOX_SPHERE_COMPOUND_CONVEX)){
+		    // Rel. position
+		    bj.position.vsub(bi.position,r);
+
+		    var boundingRadiusSum = bishape.boundingSphereRadius() + bjshape.boundingSphereRadius();
+		    if(r.norm2()<boundingRadiusSum*boundingRadiusSum){
+			pairs1.push(bi);
+			pairs2.push(bj);
+		    }
+
+		    // --- Sphere/box/compound/convexpoly versus plane ---
+		} else if((ti & BOX_SPHERE_COMPOUND_CONVEX) && (tj & types.PLANE) || (tj & BOX_SPHERE_COMPOUND_CONVEX) && (ti & types.PLANE)){
+		    var pi = (ti===PLANE) ? i : j, // Plane
+		    oi = (ti!==PLANE) ? i : j; // Other
+		    
+		    // Rel. position
+		    bodies[oi].position.vsub(bodies[pi].position,r);
+		    bodies[pi].quaternion.vmult(bodies[pi].shape.normal,normal);
+		    
+		    var q = r.dot(normal) - bodies[oi].shape.boundingSphereRadius();
+		    if(q<0.0){
+			pairs1.push(bi);
+			pairs2.push(bj);
+		    }
+		}
+	    }
 	}
-
-	  // --- Box / sphere / compound / convexpolyhedron collision ---
-	  if((ti & BOX_SPHERE_COMPOUND_CONVEX) && (tj & BOX_SPHERE_COMPOUND_CONVEX)){
-	      // Rel. position
-	      bj.position.vsub(bi.position,r);
-
-	      var boundingRadiusSum = bi.shape.boundingSphereRadius() + bj.shape.boundingSphereRadius();
-	      if(r.norm2()<boundingRadiusSum*boundingRadiusSum){
-		  pairs1.push(bi);
-		  pairs2.push(bj);
-	      }
-
-	      // --- Sphere/box/compound/convexpoly versus plane ---
-	  } else if((ti & BOX_SPHERE_COMPOUND_CONVEX) && (tj & types.PLANE) || (tj & BOX_SPHERE_COMPOUND_CONVEX) && (ti & types.PLANE)){
-	      var pi = (ti===PLANE) ? i : j, // Plane
-	      oi = (ti!==PLANE) ? i : j; // Other
-	      
-	      // Rel. position
-	      bodies[oi].position.vsub(bodies[pi].position,r);
-	      bodies[pi].quaternion.vmult(bodies[pi].shape.normal,normal);
-	      
-	      var q = r.dot(normal) - bodies[oi].shape.boundingSphereRadius();
-	      if(q<0.0){
-		  pairs1.push(bi);
-		  pairs2.push(bj);
-	      }
-	  }
-      }
     }
-  }
-  return [pairs1,pairs2];
+    return [pairs1,pairs2];
 };
 /*global CANNON:true */
 
