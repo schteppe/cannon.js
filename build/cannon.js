f821e5f4e04e8a78a53051f86c58b999dd5fc925
Bill Robinson <airbaggins@gmail.com>
2013-02-17 18:49:49 +0000

    Updated builds

--- build/cannon.js
+++ build/cannon.js
@@ -1000,11 +1000,11 @@ CANNON.Vec3.prototype.tangents = function(t1,t2){
         var n = new CANNON.Vec3(this.x/norm,
                                 this.y/norm,
                                 this.z/norm);
-        if(n.x<0.9){
-            var rand = Math.random();
+        if(Math.abs(n.x)<0.9){
+            var rand = (n.y + n.z) * 1024 % 1;
             n.cross(new CANNON.Vec3(rand,0.0000001,0).unit(),t1);
         } else
-            n.cross(new CANNON.Vec3(0.0000001,rand,0).unit(),t1);
+            n.cross(new CANNON.Vec3(0.0000001,n.x,0).unit(),t1);
         n.cross(t1,t2);
     } else {
         // The normal length is zero, make something up
@@ -1086,7 +1086,8 @@ CANNON.Vec3.prototype.almostZero = function(precision){
         Math.abs(this.z)>precision)
         return false;
     return true;
-}/*global CANNON:true */
+}
+/*global CANNON:true */
 
 /**
  * @class CANNON.Quaternion
@@ -1641,28 +1642,28 @@ CANNON.Particle = function(mass,material){
     this.allowSleep = true;
 
     // 0:awake, 1:sleepy, 2:sleeping
-    var sleepState = 0;
+    this.sleepState = 0;
 
     /**
     * @method isAwake
     * @memberof CANNON.Particle
     * @return bool
     */
-    this.isAwake = function(){ return sleepState == 0; }
+    this.isAwake = function(){ return this.sleepState == 0; }
 
     /**
     * @method isSleepy
     * @memberof CANNON.Particle
     * @return bool
     */
-    this.isSleepy = function(){ return sleepState == 1; }
+    this.isSleepy = function(){ return this.sleepState == 1; }
 
     /**
     * @method isSleeping
     * @memberof CANNON.Particle
     * @return bool
     */
-    this.isSleeping = function(){ return sleepState == 2; }
+    this.isSleeping = function(){ return this.sleepState == 2; }
 
     /**
     * @property float sleepSpeedLimit
@@ -1677,7 +1678,7 @@ CANNON.Particle = function(mass,material){
     * @brief If the body has been sleepy for this sleepTimeLimit milliseconds, it is considered sleeping.
     */
     this.sleepTimeLimit = 1000;
-    var timeLastSleepy = new Date().getTime();
+    this.timeLastSleepy = 0;
 
     /**
     * @method wakeUp
@@ -1685,7 +1686,7 @@ CANNON.Particle = function(mass,material){
     * @brief Wake the body up.
     */
     this.wakeUp = function(){
-        sleepState = 0;
+        this.sleepState = 0;
         that.dispatchEvent({type:"wakeup"});
     };
 
@@ -1695,7 +1696,7 @@ CANNON.Particle = function(mass,material){
     * @brief Force body sleep
     */
     this.sleep = function(){
-        sleepState = 2;
+        this.sleepState = 2;
     };
 
     /**
@@ -1703,16 +1704,16 @@ CANNON.Particle = function(mass,material){
     * @memberof CANNON.Particle
     * @brief Called every timestep to update internal sleep timer and change sleep state if needed.
     */
-    this.sleepTick = function(){
+    this.sleepTick = function(time){
         if(that.allowSleep){
-          if(sleepState==0 && that.velocity.norm()<that.sleepSpeedLimit){
-              sleepState = 1; // Sleepy
-              timeLastSleepy = new Date().getTime();
+          if(this.sleepState==0 && that.velocity.norm()<that.sleepSpeedLimit){
+              this.sleepState = 1; // Sleepy
+              this.timeLastSleepy = time;
               that.dispatchEvent({type:"sleepy"});
-          } else if(sleepState==1 && that.velocity.norm()>that.sleepSpeedLimit){
+          } else if(this.sleepState==1 && that.velocity.norm()>that.sleepSpeedLimit){
               that.wakeUp(); // Wake up
-          } else if(sleepState==1 && (new Date().getTime() - timeLastSleepy)>that.sleepTimeLimit){
-              sleepState = 2; // Sleeping
+          } else if(this.sleepState==1 && (time - this.timeLastSleepy)>that.sleepTimeLimit){
+              this.sleepState = 2; // Sleeping
               that.dispatchEvent({type:"sleep"});
           }
         }
@@ -3813,11 +3814,14 @@ CANNON.World.prototype.add = function(body){
     body.world = this;
     body.position.copy(body.initPosition);
     body.velocity.copy(body.initVelocity);
+    if (body instanceof CANNON.Particle) {
+        body.timeLastSleepy = this.time;
+    }
     if(body instanceof CANNON.RigidBody){
         body.angularVelocity.copy(body.initAngularVelocity);
         body.quaternion.copy(body.initQuaternion);
     }
-    
+
     // Create collision matrix
     this.collision_matrix = new Int16Array((n+1)*(n+1));
 };
@@ -4365,7 +4369,7 @@ CANNON.World.prototype.step = function(dt){
     // Sleeping update
     if(world.allowSleep){
         for(var i=0; i<N; i++){
-           bodies[i].sleepTick();
+           bodies[i].sleepTick(this.time);
         }
     }
 };
