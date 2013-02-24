c9958c83613524c38bceb58874583b67bc1ab63e
Stefan Hedman <schteppe@gmail.com>
2013-02-24 17:02:52 +0100

    GC improve SplitSolver

--- src/solver/SplitSolver.js
+++ src/solver/SplitSolver.js
@@ -5,16 +5,31 @@ CANNON.SplitSolver = function(subsolver){
 CANNON.SplitSolver.prototype = new CANNON.Solver();
 
 // Returns the number of subsystems
+var SplitSolver_solve_nodes = []; // All allocated node objects
+var SplitSolver_solve_eqs = [];   // Temp array
+var SplitSolver_solve_bds = [];   // Temp array
+var SplitSolver_solve_dummyWorld = {bodies:null}; // Temp object
 CANNON.SplitSolver.prototype.solve = function(dt,world){
-    var nodes=[],
+    var nodes=SplitSolver_solve_nodes,
         bodies=world.bodies,
         equations=this.equations,
         Neq=equations.length,
         Nbodies=bodies.length,
         subsolver=this.subsolver;
-    for(var i=0; i<Nbodies; i++)
+    // Create needed nodes, reuse if possible
+    for(var i=nodes.length; i!==Nbodies; i++){
         nodes.push({ body:bodies[i], children:[], eqs:[], visited:false });
-    for(var k=0; k<Neq; k++){
+    }
+
+    // Reset node values
+    for(var i=0; i!==Nbodies; i++){
+        var node = nodes[i];
+        node.body = bodies[i];
+        node.children.length = 0;
+        node.eqs.length = 0;
+        node.visited = false;
+    }
+    for(var k=0; k!==Neq; k++){
         var eq=equations[k],
             i=bodies.indexOf(eq.bi),
             j=bodies.indexOf(eq.bj),
@@ -28,11 +43,12 @@ CANNON.SplitSolver.prototype.solve = function(dt,world){
 
     var STATIC = CANNON.Body.STATIC;
     function getUnvisitedNode(nodes){
-        var N = nodes.length;
-        for(var i=0; i<N; i++){
+        var Nnodes = nodes.length;
+        for(var i=0; i!==Nnodes; i++){
             var node = nodes[i];
-            if(!node.visited && !(node.body.motionstate & STATIC))
+            if(!node.visited && !(node.body.motionstate & STATIC)){
                 return node;
+            }
         }
         return false;
     }
@@ -54,20 +70,30 @@ CANNON.SplitSolver.prototype.solve = function(dt,world){
         }
     }
 
-    var child, n=0;
+    var child, n=0, eqs=SplitSolver_solve_eqs, bds=SplitSolver_solve_bds;
+    function visitFunc(node){
+        bds.push(node.body);
+        var Neqs = node.eqs.length;
+        for(var i=0; i!==Neqs; i++){
+            var eq = node.eqs[i];
+            if(eqs.indexOf(eq) === -1){
+                eqs.push(eq);
+            }
+        }
+    }
+    var dummyWorld = SplitSolver_solve_dummyWorld;
     while((child = getUnvisitedNode(nodes))){
-        var eqs=[], bds=[];
-        bfs(child,function(node){
-            bds.push(node.body);
-            for(var i=0; i<node.eqs.length; i++)
-                if(eqs.indexOf(node.eqs[i]) == -1)
-                    eqs.push(node.eqs[i]);
-        });
+        eqs.length = 0;
+        bds.length = 0;
+        bfs(child,visitFunc);
 
-        for(var i=0; i<eqs.length; i++)
+        var Neqs = eqs.length;
+        for(var i=0; i!==Neqs; i++){
             subsolver.addEquation(eqs[i]);
+        }
 
-        var iter = subsolver.solve(dt,{bodies:bds});
+        dummyWorld.bodies = bds;
+        var iter = subsolver.solve(dt,dummyWorld);
         subsolver.removeAllEquations();
         n++;
     }
