b1f53bca0b45081acb66fe4ec3e95d4043f80477
Stefan Hedman <schteppe@gmail.com>
2014-02-13 21:29:25 +0100

    cleaned up hinge constraint

--- src/constraints/HingeConstraint.js
+++ src/constraints/HingeConstraint.js
@@ -23,6 +23,7 @@ function HingeConstraint(bodyA, pivotA, axisA, bodyB, pivotB, axisB, maxForce){
 
     maxForce = maxForce || 1e6;
     var that = this;
+
     // Equations to be fed to the solver
     var eqs = this.equations = [
         new RotationalEquation(bodyA,bodyB), // rotational1
@@ -32,11 +33,10 @@ function HingeConstraint(bodyA, pivotA, axisA, bodyB, pivotB, axisB, maxForce){
         new ContactEquation(bodyA,bodyB),    // p2pTangent2
     ];
 
-    this.getRotationalEquation1 =   function(){ return eqs[0]; };
-    this.getRotationalEquation2 =   function(){ return eqs[1]; };
-    this.getPointToPointEquation1 = function(){ return eqs[2]; };
-    this.getPointToPointEquation2 = function(){ return eqs[3]; };
-    this.getPointToPointEquation3 = function(){ return eqs[4]; };
+    this.pivotA = pivotA;
+    this.pivotB = pivotB;
+    this.axisA = axisA;
+    this.axisB = axisB;
 
     var r1 =        this.getRotationalEquation1();
     var r2 =        this.getRotationalEquation2();
@@ -51,9 +51,9 @@ function HingeConstraint(bodyA, pivotA, axisA, bodyB, pivotB, axisB, maxForce){
     var unitPivotA = pivotA.unit();
     var unitPivotB = pivotB.unit();
 
-    var axisA_x_pivotA = new Vec3();
-    var axisA_x_axisA_x_pivotA = new Vec3();
-    var axisB_x_pivotB = new Vec3();
+    var axisA_x_pivotA = this.axisA_x_pivotA = new Vec3();
+    var axisA_x_axisA_x_pivotA = this.axisA_x_axisA_x_pivotA = new Vec3();
+    var axisB_x_pivotB = this.axisB_x_pivotB = new Vec3();
     axisA.cross(unitPivotA,axisA_x_pivotA);
     if(axisA_x_pivotA.norm2() < 0.001){ // pivotA is along the same line as axisA
         unitPivotA.tangents(axisA_x_pivotA,axisA_x_pivotA);
@@ -68,57 +68,73 @@ function HingeConstraint(bodyA, pivotA, axisA, bodyB, pivotB, axisB, maxForce){
     axisB_x_pivotB.normalize();
 
     // Motor stuff
-    var motorEnabled = false;
+    this.motorEnabled = false;
     this.motorTargetVelocity = 0;
     this.motorMinForce = -maxForce;
     this.motorMaxForce = maxForce;
-    this.enableMotor = function(){
-        if(!motorEnabled){
-            motor = new RotationalMotorEquation(bodyA,bodyB,maxForce);
-            eqs.push(motor);
-            motorEnabled = true;
-        }
-    };
-    this.disableMotor = function(){
-        if(motorEnabled){
-            motorEnabled = false;
-            motor = null;
-            eqs.pop();
-        }
-    };
-
-    // Update
-    this.update = function(){
-        // Update world positions of pivots
-        /*
-        bodyB.position.vsub(bodyA.position,normal.ni);
-        normal.ni.normalize();
-        */
-        normal.ni.set(1,0,0);
-        t1.ni.set(0,1,0);
-        t2.ni.set(0,0,1);
-        bodyA.quaternion.vmult(pivotA,normal.ri);
-        bodyB.quaternion.vmult(pivotB,normal.rj);
-
-        //normal.ni.tangents(t1.ni,t2.ni);
-        normal.ri.copy(t1.ri);
-        normal.rj.copy(t1.rj);
-        normal.ri.copy(t2.ri);
-        normal.rj.copy(t2.rj);
-
-        // update rotational constraints
-        bodyA.quaternion.vmult(axisA_x_pivotA, r1.ni);
-        bodyB.quaternion.vmult(axisB,          r1.nj);
-        bodyA.quaternion.vmult(axisA_x_axisA_x_pivotA,  r2.ni);
-        bodyB.quaternion.vmult(axisB,           r2.nj);
-
-        if(motorEnabled){
-            bodyA.quaternion.vmult(axisA,motor.axisA);
-            bodyB.quaternion.vmult(axisB,motor.axisB);
-            motor.targetVelocity = that.motorTargetVelocity;
-            motor.maxForce = that.motorMaxForce;
-            motor.minForce = that.motorMinForce;
-        }
-    };
+    this.motorEquation = new RotationalMotorEquation(bodyA,bodyB,maxForce);
 };
 HingeConstraint.prototype = new Constraint();
+
+HingeConstraint.prototype.enableMotor = function(){
+    if(!this.motorEnabled){
+        this.equations.push(this.motorEquation);
+        this.motorEnabled = true;
+    }
+};
+
+HingeConstraint.prototype.disableMotor = function(){
+    if(this.motorEnabled){
+        this.motorEnabled = false;
+        this.equations.pop();
+    }
+};
+
+HingeConstraint.prototype.update = function(){
+    var bodyA = this.bodyA,
+        bodyB = this.bodyB,
+        eqs = this.equations,
+        motor = this.motorEquation,
+        r1 = eqs[0],
+        r2 = eqs[1],
+        normal = eqs[2],
+        t1 = eqs[3],
+        t2 = eqs[4];
+
+    // Update world positions of pivots
+    /*
+    bodyB.position.vsub(bodyA.position,normal.ni);
+    normal.ni.normalize();
+    */
+    normal.ni.set(1,0,0);
+    t1.ni.set(0,1,0);
+    t2.ni.set(0,0,1);
+    bodyA.quaternion.vmult(this.pivotA,normal.ri);
+    bodyB.quaternion.vmult(this.pivotB,normal.rj);
+
+    //normal.ni.tangents(t1.ni,t2.ni);
+    normal.ri.copy(t1.ri);
+    normal.rj.copy(t1.rj);
+    normal.ri.copy(t2.ri);
+    normal.rj.copy(t2.rj);
+
+    // update rotational constraints
+    bodyA.quaternion.vmult(this.axisA_x_pivotA,         r1.ni);
+    bodyB.quaternion.vmult(this.axisB,                  r1.nj);
+    bodyA.quaternion.vmult(this.axisA_x_axisA_x_pivotA, r2.ni);
+    bodyB.quaternion.vmult(this.axisB,                  r2.nj);
+
+    if(this.motorEnabled){
+        bodyA.quaternion.vmult(this.axisA,motor.axisA);
+        bodyB.quaternion.vmult(this.axisB,motor.axisB);
+        motor.targetVelocity = this.motorTargetVelocity;
+        motor.maxForce = this.motorMaxForce;
+        motor.minForce = this.motorMinForce;
+    }
+};
+
+HingeConstraint.prototype.getRotationalEquation1 =   function(){ return this.equations[0]; };
+HingeConstraint.prototype.getRotationalEquation2 =   function(){ return this.equations[1]; };
+HingeConstraint.prototype.getPointToPointEquation1 = function(){ return this.equations[2]; };
+HingeConstraint.prototype.getPointToPointEquation2 = function(){ return this.equations[3]; };
+HingeConstraint.prototype.getPointToPointEquation3 = function(){ return this.equations[4]; };
