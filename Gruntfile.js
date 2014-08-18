cfa4be397b00880695e8cfb7861c35b4c6af60c8
Stefan Hedman <schteppe@gmail.com>
2014-08-18 17:16:22 +0200

    made it possible to run tests with grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,13 +47,19 @@ module.exports = function(grunt) {
                 },
             }
         },
+
+        nodeunit: {
+            all: ['test/**/*.js'],
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.registerTask('default', ['concat', 'browserify', 'uglify', 'addLicense']);
+    grunt.registerTask('test', ['nodeunit']);
 
     grunt.registerTask('addLicense','Adds the LICENSE to the top of the built files',function(){
         var text = fs.readFileSync("LICENSE").toString();
