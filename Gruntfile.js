a7c4231012e848e7fffaf49e4490ece94f685565
Stefan Hedman <schteppe@gmail.com>
2014-09-22 13:55:43 +0200

    make bundles ignore the define() function from require.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-browserify');
-    grunt.registerTask('default', ['concat', 'browserify', 'uglify', 'addLicense']);
+    grunt.registerTask('default', ['concat', 'browserify', 'uglify', 'addLicense', 'requireJsFix']);
     grunt.registerTask('test', ['nodeunit']);
 
     grunt.registerTask('addLicense','Adds the LICENSE to the top of the built files',function(){
@@ -71,5 +71,14 @@ module.exports = function(grunt) {
 
         fs.writeFileSync(bundlePath,text+"\n"+dev);
         fs.writeFileSync(minifiedBundlePath,text+"\n"+min);
+    });
+
+    // Not sure what flag Browserify needs to do this. Fixing it manually for now.
+    grunt.registerTask('requireJsFix','Modifies the browserify bundle so it works with RequireJS',function(){
+        [bundlePath, minifiedBundlePath].forEach(function(path){
+            var text = fs.readFileSync(path).toString();
+            text = text.replace('define.amd', 'false'); // This makes the bundle skip using define() from RequireJS
+            fs.writeFileSync(bundlePath, text);
+        });
     });
 };
