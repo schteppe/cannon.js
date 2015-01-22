a2934adc1f5352124b855788367a4f51e8a0a6b9
Stefan Hedman <schteppe@gmail.com>
2015-01-22 17:16:19 +0100

    simplified HingeConstraint code a lot and removed its getters for equations

--- src/constraints/HingeConstraint.js
+++ src/constraints/HingeConstraint.js
@@ -8,7 +8,7 @@ var ContactEquation = require('../equations/ContactEquation');
 var Vec3 = require('../math/Vec3');
 
 /**
- * Hinge constraint. Defines an axis locally in each body, and tries to keep these equal in world space.
+ * Hinge constraint. Think of it as a door hinge. It tries to keep the door in the correct place and with the correct orientation.
  * @class HingeConstraint
  * @constructor
  * @author schteppe
@@ -16,7 +16,7 @@ var Vec3 = require('../math/Vec3');
  * @param {Body} bodyB
  * @param {object} [options]
  * @param {Vec3} [options.pivotA] A point defined locally in bodyA. This defines the offset of axisA.
- * @param {Vec3} [options.axisA] an axis that bodyA can rotate around.
+ * @param {Vec3} [options.axisA] An axis that bodyA can rotate around, defined locally in bodyA.
  * @param {Vec3} [options.pivotB]
  * @param {Vec3} [options.axisB]
  * @param {Number} [options.maxForce=1e6]
@@ -34,12 +34,14 @@ function HingeConstraint(bodyA, bodyB, options){
      * @property {Vec3} axisA
      */
     var axisA = this.axisA = options.axisA ? options.axisA.clone() : new Vec3(1,0,0);
+    axisA.normalize();
 
     /**
      * Rotation axis, defined locally in bodyB.
      * @property {Vec3} axisB
      */
     var axisB = this.axisB = options.axisB ? options.axisB.clone() : new Vec3(1,0,0);
+    axisB.normalize();
 
     /**
      * @property {RotationalEquation} rotationalEquation1
@@ -63,26 +65,6 @@ function HingeConstraint(bodyA, bodyB, options){
         r2, // rotational2
         motor
     );
-
-    var unitPivotA = pivotA.unit();
-    var unitPivotB = pivotB.unit();
-
-    var axisA_x_pivotA = this.axisA_x_pivotA = new Vec3();
-    var axisA_x_axisA_x_pivotA = this.axisA_x_axisA_x_pivotA = new Vec3();
-    var axisB_x_pivotB = this.axisB_x_pivotB = new Vec3();
-    axisA.cross(unitPivotA,axisA_x_pivotA);
-    if(axisA_x_pivotA.norm2() < 0.001){ // pivotA is along the same line as axisA
-        unitPivotA.tangents(axisA_x_pivotA,axisA_x_pivotA);
-    }
-    axisA.cross(axisA_x_pivotA,axisA_x_axisA_x_pivotA);
-    axisB.cross(unitPivotB,axisB_x_pivotB);
-    if(axisB_x_pivotB.norm2() < 0.001){ // pivotB is along the same line as axisB
-        axisB.tangents(axisB_x_pivotB,axisB_x_pivotB);
-    }
-
-    axisA_x_pivotA.normalize();
-    axisB_x_pivotB.normalize();
-
 }
 HingeConstraint.prototype = new PointToPointConstraint();
 HingeConstraint.constructor = HingeConstraint;
@@ -118,42 +100,30 @@ HingeConstraint.prototype.setMotorMaxForce = function(maxForce){
     this.motorEquation.minForce = -maxForce;
 };
 
+var HingeConstraint_update_tmpVec1 = new Vec3();
+var HingeConstraint_update_tmpVec2 = new Vec3();
+
 HingeConstraint.prototype.update = function(){
     var bodyA = this.bodyA,
         bodyB = this.bodyB,
-        eqs = this.equations,
         motor = this.motorEquation,
         r1 = this.rotationalEquation1,
-        r2 = this.rotationalEquation2;
+        r2 = this.rotationalEquation2,
+        worldAxisA = HingeConstraint_update_tmpVec1,
+        worldAxisB = HingeConstraint_update_tmpVec2;
+
+    var axisA = this.axisA;
+    var axisB = this.axisB;
 
     PointToPointConstraint.prototype.update.call(this);
 
-    var axisA_x_pivotA = this.axisA_x_pivotA;
-    var axisA = this.axisA;
-    var axisB = this.axisB;
-    var pivotA = this.pivotA;
-    var pivotB = this.pivotB;
-    var axisA_x_axisA_x_pivotA = this.axisA_x_axisA_x_pivotA;
-    var axisB_x_pivotB = this.axisB_x_pivotB;
+    // Get world axes
+    bodyA.quaternion.vmult(axisA, worldAxisA);
+    bodyB.quaternion.vmult(axisB, worldAxisB);
 
-    axisA.cross(pivotA, axisA_x_pivotA);
-    if(axisA_x_pivotA.norm2() < 0.001){ // pivotA is along the same line as axisA
-        pivotA.tangents(axisA_x_pivotA, axisA_x_pivotA);
-    }
-    axisA.cross(axisA_x_pivotA, axisA_x_axisA_x_pivotA);
-    axisB.cross(pivotB, axisB_x_pivotB);
-    if(axisB_x_pivotB.norm2() < 0.001){ // pivotB is along the same line as axisB
-        axisB.tangents(axisB_x_pivotB,axisB_x_pivotB);
-    }
-
-    axisA_x_pivotA.normalize();
-    axisB_x_pivotB.normalize();
-
-    // update rotational constraints
-    bodyA.quaternion.vmult(axisA_x_pivotA, r1.axisA);
-    bodyB.quaternion.vmult(axisB, r1.axisB);
-    bodyA.quaternion.vmult(axisA_x_axisA_x_pivotA, r2.axisA);
-    bodyB.quaternion.vmult(axisB, r2.axisB);
+    worldAxisA.tangents(r1.axisA, r2.axisA);
+    r1.axisB.copy(worldAxisB);
+    r2.axisB.copy(worldAxisB);
 
     if(this.motorEquation.enabled){
         bodyA.quaternion.vmult(this.axisA, motor.axisA);
@@ -161,8 +131,3 @@ HingeConstraint.prototype.update = function(){
     }
 };
 
-HingeConstraint.prototype.getRotationalEquation1 =   function(){ return this.rotationalEquation1; };
-HingeConstraint.prototype.getRotationalEquation2 =   function(){ return this.rotationalEquation2; };
-HingeConstraint.prototype.getPointToPointEquation1 = function(){ return this.equationX; };
-HingeConstraint.prototype.getPointToPointEquation2 = function(){ return this.equationY; };
-HingeConstraint.prototype.getPointToPointEquation3 = function(){ return this.equationZ; };
