868ceb71cf5ab37e8eb550cc20d8dbda6286c2cd
feng <908087098@qq.com>
2019-09-27 19:33:53 +0800

    Utils.ts

---
+++ src/utils/Utils.ts
@@ -0,0 +1,25 @@
+namespace cannon
+{
+    export class Utils
+    {
+
+        /**
+         * Extend an options object with default values.
+         * @param  options The options object. May be falsy: in this case, a new object is created and returned.
+         * @param  defaults An object containing default values.
+         * @return The modified options object.
+         */
+        static defaults(options: Object, defaults: Object = {})
+        {
+            for (var key in defaults)
+            {
+                if (!(key in options))
+                {
+                    options[key] = defaults[key];
+                }
+            }
+
+            return options;
+        }
+    }
+}
