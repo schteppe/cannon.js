0fcc20579386bfb6c90e2f7dec6f90c6261b048a
feng <908087098@qq.com>
2019-09-27 22:06:16 +0800

    GSSolver.ts

---
+++ src/solver/Solver.ts
@@ -0,0 +1,65 @@
+namespace cannon
+{
+    export class Solver
+    {
+        /**
+         * All equations to be solved
+         */
+        equations: [];
+
+        /**
+         * Constraint equation solver base class.
+         * @author schteppe / https://github.com/schteppe
+         */
+        constructor()
+        {
+            this.equations = [];
+        }
+
+        /**
+         * Should be implemented in subclasses!
+         * @param dt
+         * @param world
+         */
+        solve(dt: number, world: World)
+        {
+            // Should return the number of iterations done!
+            return 0;
+        }
+
+        /**
+         * Add an equation
+         * @param eq
+         */
+        addEquation(eq: Equation)
+        {
+            if (eq.enabled)
+            {
+                this.equations.push(eq);
+            }
+        }
+
+        /**
+         * Remove an equation
+         * @param eq
+         */
+        removeEquation(eq: Equation)
+        {
+            var eqs = this.equations;
+            var i = eqs.indexOf(eq);
+            if (i !== -1)
+            {
+                eqs.splice(i, 1);
+            }
+        }
+
+        /**
+         * Add all equations
+         */
+        removeAllEquations()
+        {
+            this.equations.length = 0;
+        }
+
+    }
+}
