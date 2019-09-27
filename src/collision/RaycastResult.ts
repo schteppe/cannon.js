da6b20b1b5aea7a03bea2703898353549abb77e9
feng <908087098@qq.com>
2019-09-27 20:08:21 +0800

    Shape.ts

---
+++ src/collision/RaycastResult.ts
@@ -0,0 +1,83 @@
+namespace cannon
+{
+	export class RaycastResult
+	{
+		rayFromWorld = new Vec3();
+
+		rayToWorld = new Vec3();
+
+		hitNormalWorld = new Vec3();
+
+		hitPointWorld = new Vec3();
+
+		hasHit = false;
+
+		shape = null;
+
+		body = null;
+
+		/**
+		 * The index of the hit triangle, if the hit shape was a trimesh.
+		 */
+		hitFaceIndex = -1;
+
+		/**
+		 * Distance to the hit. Will be set to -1 if there was no hit.
+		 */
+		distance = -1;
+
+		/**
+		 * If the ray should stop traversing the bodies.
+		 */
+		private _shouldStop = false;
+
+		/**
+		 * Storage for Ray casting data.
+		 */
+		constructor()
+		{
+
+		}
+
+		/**
+		 * Reset all result data.
+		 */
+		reset()
+		{
+			this.rayFromWorld.setZero();
+			this.rayToWorld.setZero();
+			this.hitNormalWorld.setZero();
+			this.hitPointWorld.setZero();
+			this.hasHit = false;
+			this.shape = null;
+			this.body = null;
+			this.hitFaceIndex = -1;
+			this.distance = -1;
+			this._shouldStop = false;
+		};
+
+		abort()
+		{
+			this._shouldStop = true;
+		};
+
+		set(
+			rayFromWorld: Vec3,
+			rayToWorld: Vec3,
+			hitNormalWorld: Vec3,
+			hitPointWorld: Vec3,
+			shape: Shape,
+			body: Body,
+			distance: number
+		)
+		{
+			this.rayFromWorld.copy(rayFromWorld);
+			this.rayToWorld.copy(rayToWorld);
+			this.hitNormalWorld.copy(hitNormalWorld);
+			this.hitPointWorld.copy(hitPointWorld);
+			this.shape = shape;
+			this.body = body;
+			this.distance = distance;
+		};
+	}
+}
---
+++ src/shapes/Shape.ts
@@ -0,0 +1,99 @@
+namespace cannon
+{
+    export class Shape
+    {
+        /**
+         * Identifyer of the Shape.
+         */
+        id: number;
+
+        /**
+         * The type of this shape. Must be set to an int > 0 by subclasses.
+         */
+        type: number;
+
+        /**
+         * The local bounding sphere radius of this shape.
+         */
+        boundingSphereRadius: number;
+
+        /**
+         * Whether to produce contact forces when in contact with other bodies. Note that contacts will be generated, but they will be disabled.
+         */
+        collisionResponse: boolean;
+
+        collisionFilterGroup: number;
+
+        collisionFilterMask: number;
+
+        material: Material;
+
+        body: Body;
+
+        /**
+         * Base class for shapes
+         * 
+         * @param options 
+         * @author schteppe
+         */
+        constructor(options: { type?: number, collisionFilterGroup?: number, collisionFilterMask?: number, collisionResponse?: boolean, material?: any } = {})
+        {
+            this.id = Shape.idCounter++;
+            this.type = options.type || 0;
+
+            this.boundingSphereRadius = 0;
+            this.collisionResponse = options.collisionResponse ? options.collisionResponse : true;
+
+            this.collisionFilterGroup = options.collisionFilterGroup !== undefined ? options.collisionFilterGroup : 1;
+
+            this.collisionFilterMask = options.collisionFilterMask !== undefined ? options.collisionFilterMask : -1;
+
+            this.material = options.material ? options.material : null;
+            this.body = null;
+        }
+
+        /**
+         * Computes the bounding sphere radius. The result is stored in the property .boundingSphereRadius
+         */
+        updateBoundingSphereRadius()
+        {
+            throw "computeBoundingSphereRadius() not implemented for shape type " + this.type;
+        };
+
+        /**
+         * Get the volume of this shape
+         */
+        volume()
+        {
+            throw "volume() not implemented for shape type " + this.type;
+        };
+
+        /**
+         * Calculates the inertia in the local frame for this shape.
+         * @param mass
+         * @param target
+         * @see http://en.wikipedia.org/wiki/List_of_moments_of_inertia
+         */
+        calculateLocalInertia(mass: number, target: Vec3)
+        {
+            throw "calculateLocalInertia() not implemented for shape type " + this.type;
+        };
+
+        static idCounter = 0;
+
+        /**
+         * The available shape types.
+         */
+        static types = {
+            SPHERE: 1,
+            PLANE: 2,
+            BOX: 4,
+            COMPOUND: 8,
+            CONVEXPOLYHEDRON: 16,
+            HEIGHTFIELD: 32,
+            PARTICLE: 64,
+            CYLINDER: 128,
+            TRIMESH: 256
+        };
+    }
+}
