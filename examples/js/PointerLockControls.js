7895b33d3f5a3578a84e0933ba884deb9d09a32d
feng <908087098@qq.com>
2019-09-27 12:33:51 +0800

    还原 示例threejs_fps

--- examples/js/PointerLockControls.js
+++ examples/js/PointerLockControls.js
@@ -1,24 +1,22 @@
-/// <reference path="../../libs/feng3d.d.ts" />
-
 /**
  * @author mrdoob / http://mrdoob.com/
  * @author schteppe / https://github.com/schteppe
  */
-var PointerLockControls = function (camera, cannonBody)
-{
+var PointerLockControls = function ( camera, cannonBody ) {
+
     var eyeYPos = 2; // eyes are 2 meters above the ground
     var velocityFactor = 0.2;
     var jumpVelocity = 20;
     var scope = this;
 
-    var pitchObject = new feng3d.GameObject();
-    pitchObject.addChild(camera.gameObject);
+    var pitchObject = new THREE.Object3D();
+    pitchObject.add( camera );
 
-    var yawObject = new feng3d.GameObject();
-    yawObject.transform.y = 2;
-    yawObject.addChild(pitchObject);
+    var yawObject = new THREE.Object3D();
+    yawObject.position.y = 2;
+    yawObject.add( pitchObject );
 
-    var quat = new feng3d.Quaternion();
+    var quat = new THREE.Quaternion();
 
     var moveForward = false;
     var moveBackward = false;
@@ -28,20 +26,19 @@ var PointerLockControls = function (camera, cannonBody)
     var canJump = false;
 
     var contactNormal = new CANNON.Vec3(); // Normal in the contact, pointing *out* of whatever the player touched
-    var upAxis = new CANNON.Vec3(0, 1, 0);
-    cannonBody.addEventListener("collide", function (e)
-    {
+    var upAxis = new CANNON.Vec3(0,1,0);
+    cannonBody.addEventListener("collide",function(e){
         var contact = e.contact;
 
         // contact.bi and contact.bj are the colliding bodies, and contact.ni is the collision normal.
         // We do not yet know which one is which! Let's check.
-        if (contact.bi.id == cannonBody.id)  // bi is the player body, flip the contact normal
+        if(contact.bi.id == cannonBody.id)  // bi is the player body, flip the contact normal
             contact.ni.negate(contactNormal);
         else
             contactNormal.copy(contact.ni); // bi is something else. Keep the normal as it is
 
         // If contactNormal.dot(upAxis) is between 0 and 1, we know that the contact normal is somewhat in the up direction.
-        if (contactNormal.dot(upAxis) > 0.5) // Use a "good" threshold value between 0 and 1 here!
+        if(contactNormal.dot(upAxis) > 0.5) // Use a "good" threshold value between 0 and 1 here!
             canJump = true;
     });
 
@@ -49,10 +46,9 @@ var PointerLockControls = function (camera, cannonBody)
 
     var PI_2 = Math.PI / 2;
 
-    var onMouseMove = function (event)
-    {
+    var onMouseMove = function ( event ) {
 
-        if (scope.enabled === false) return;
+        if ( scope.enabled === false ) return;
 
         var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
         var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
@@ -60,14 +56,12 @@ var PointerLockControls = function (camera, cannonBody)
         yawObject.rotation.y -= movementX * 0.002;
         pitchObject.rotation.x -= movementY * 0.002;
 
-        pitchObject.rotation.x = Math.max(- PI_2, Math.min(PI_2, pitchObject.rotation.x));
+        pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
     };
 
-    var onKeyDown = function (event)
-    {
+    var onKeyDown = function ( event ) {
 
-        switch (event.keyCode)
-        {
+        switch ( event.keyCode ) {
 
             case 38: // up
             case 87: // w
@@ -89,8 +83,7 @@ var PointerLockControls = function (camera, cannonBody)
                 break;
 
             case 32: // space
-                if (canJump === true)
-                {
+                if ( canJump === true ){
                     velocity.y = jumpVelocity;
                 }
                 canJump = false;
@@ -99,11 +92,9 @@ var PointerLockControls = function (camera, cannonBody)
 
     };
 
-    var onKeyUp = function (event)
-    {
+    var onKeyUp = function ( event ) {
 
-        switch (event.keyCode)
-        {
+        switch( event.keyCode ) {
 
             case 38: // up
             case 87: // w
@@ -129,54 +120,51 @@ var PointerLockControls = function (camera, cannonBody)
 
     };
 
-    document.addEventListener('mousemove', onMouseMove, false);
-    document.addEventListener('keydown', onKeyDown, false);
-    document.addEventListener('keyup', onKeyUp, false);
+    document.addEventListener( 'mousemove', onMouseMove, false );
+    document.addEventListener( 'keydown', onKeyDown, false );
+    document.addEventListener( 'keyup', onKeyUp, false );
 
     this.enabled = false;
 
-    this.getObject = function ()
-    {
+    this.getObject = function () {
         return yawObject;
     };
 
-    this.getDirection = function (targetVec)
-    {
-        targetVec.set(0, 0, -1);
+    this.getDirection = function(targetVec){
+        targetVec.set(0,0,-1);
         quat.multiplyVector3(targetVec);
     }
 
     // Moves the camera to the Cannon.js object position and adds velocity to the object if the run key is down
-    var inputVelocity = new feng3d.Vector3();
-    this.update = function (delta)
-    {
+    var inputVelocity = new THREE.Vector3();
+    var euler = new THREE.Euler();
+    this.update = function ( delta ) {
 
-        if (scope.enabled === false) return;
+        if ( scope.enabled === false ) return;
 
         delta *= 0.1;
 
-        inputVelocity.set(0, 0, 0);
+        inputVelocity.set(0,0,0);
 
-        if (moveForward)
-        {
+        if ( moveForward ){
             inputVelocity.z = -velocityFactor * delta;
         }
-        if (moveBackward)
-        {
+        if ( moveBackward ){
             inputVelocity.z = velocityFactor * delta;
         }
 
-        if (moveLeft)
-        {
+        if ( moveLeft ){
             inputVelocity.x = -velocityFactor * delta;
         }
-        if (moveRight)
-        {
+        if ( moveRight ){
             inputVelocity.x = velocityFactor * delta;
         }
 
         // Convert velocity to world coordinates
-        quat.fromEulerAngles(pitchObject.rotation.x, yawObject.rotation.y, 0);
+        euler.x = pitchObject.rotation.x;
+        euler.y = yawObject.rotation.y;
+        euler.order = "XYZ";
+        quat.setFromEuler(euler);
         inputVelocity.applyQuaternion(quat);
         //quat.multiplyVector3(inputVelocity);
 
@@ -184,6 +172,6 @@ var PointerLockControls = function (camera, cannonBody)
         velocity.x += inputVelocity.x;
         velocity.z += inputVelocity.z;
 
-        yawObject.transform.position.copy(cannonBody.transform.position);
+        yawObject.position.copy(cannonBody.position);
     };
 };
