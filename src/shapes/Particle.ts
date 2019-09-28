3bf9ff04c5d88a1022bfa5d6df3c17899acc0250
feng <908087098@qq.com>
2019-09-28 08:56:17 +0800

    Particle.ts

---
+++ src/shapes/Particle.ts
@@ -0,0 +1,47 @@
+namespace cannon
+{
+    export class Particle extends Shape
+    {
+
+        /**
+         * Particle shape.
+         * 
+         * @author schteppe
+         */
+        constructor()
+        {
+            super({
+                type: Shape.types.PARTICLE
+            });
+        }
+
+        /**
+         * @param mass
+         * @param target
+         */
+        calculateLocalInertia(mass: number, target: Vec3)
+        {
+            target = target || new Vec3();
+            target.set(0, 0, 0);
+            return target;
+        };
+
+        volume()
+        {
+            return 0;
+        };
+
+        updateBoundingSphereRadius()
+        {
+            this.boundingSphereRadius = 0;
+        }
+
+        calculateWorldAABB(pos, quat, min, max)
+        {
+            // Get each axis max
+            min.copy(pos);
+            max.copy(pos);
+        };
+    }
+
+}
